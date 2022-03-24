ae9f79c2984c19c655ba7fe6480ec713f8fd322d
h3d-longnsp <hydroxyzine@metacity.inc>
2022-03-24 09:39:49 +0700

    sign-off commit
    Signed-off-by: h3d-longnsp <hydroxyzine@metacity.inc>

--- pycharmm.py
+++ pycharmm.py
@@ -4,7 +4,7 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
-
+        # git config --global format.signoff true
 
 if __name__ == '__main__':
     unittest.main()
