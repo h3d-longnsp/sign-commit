17f0c5c0d1b0ce359fab966f74810ee9a4ebbdce
LonggNguyen89 <hydroxyzine@metacity.inc>
2022-03-29 23:58:39 +0700

    commit in my laptop

--- pycharmm.py
+++ pycharmm.py
@@ -5,6 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
         # git config --global format.signoff true
+        # config smt
 
 if __name__ == '__main__':
     unittest.main()
